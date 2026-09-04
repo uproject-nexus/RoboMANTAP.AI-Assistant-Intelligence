9d32437659b27a52cb03db5afa0f9f8fec038898
U.Project Nexus <uproject.nexus@gmail.com>
2026-09-04 17:23:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -284,7 +284,7 @@ if st.session_state.page == "landing":
     st.markdown("""
     <div class="guru-card">
         <h2 style="margin:0;">🔴 Live Monitoring & AI Generator</h2>
-        <p style="font-size: 14px; opacity:0.8; margin-top:5px;">Pantau skor siswa secara real-time, generate soal, dan integrasi WhatsApp.</p>
+        <p style="font-size: 9px; opacity:0.8; margin-top:5px;">Pantau skor siswa secara real-time, generate soal, dan integrasi WhatsApp.</p>
     </div>
     """, unsafe_allow_html=True)
     if st.button("🔒 Masuk Portal Guru ➔", use_container_width=True):
