3f0de53167c0a1d0525c37906d185357711dd72c
U.Project Nexus <uproject.nexus@gmail.com>
2026-09-08 20:17:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1699,7 +1699,7 @@ elif st.session_state.page == "guru_dashboard":
         # --- CSS AGAR 3 TOMBOL TETAP SEJAJAR & TIDAK MELAR DI HP ---
         st.markdown("""
         <style>
-        @media (max-width: 640px) {
+        @media (max-width: 350px) {
             /* Paksa kontainer kolom tetap horizontal (1 baris) */
             div[data-testid="stHorizontalBlock"] {
                 display: flex !important;
