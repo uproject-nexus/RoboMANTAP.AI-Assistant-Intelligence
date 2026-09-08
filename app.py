b3fecc2398dc29760ec80c4211c0a2c5893e1e59
U.Project Nexus <uproject.nexus@gmail.com>
2026-09-08 13:24:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -427,7 +427,7 @@ def generate_quiz_docx(config: dict, quiz_list: list) -> bytes:
     r2.bold = True
     r2.font.size = Pt(10.5)
     
-    r3 = p_title.add_run("Tahun Ajaran: ......... / .........")
+    r3 = p_title.add_run("Tahun Ajaran: ............ / ............")
     r3.italic = True
     r3.font.size = Pt(9.5)
     r3.font.color.rgb = RGBColor(100, 100, 100)
