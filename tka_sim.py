b2d53ce8c372020ceb98b8cb2c3f24a9c4a7cca0
U.Project Nexus <uproject.nexus@gmail.com>
2026-09-12 22:04:17 +0700

    Update tka_sim.py

--- tka_sim.py
+++ tka_sim.py
@@ -6,8 +6,8 @@ def inject_tka_css():
     <style>
         /* Paksa 3 kolom tombol bawah tetap sejajar di HP */
         .bottom-nav-container [data-testid="column"] {
-            min-width: 5% !important;
-            padding: 0px 2px !important;
+            min-width: 3% !important;
+            padding: 0px 0.5px !important;
         }
         
         /* Tombol Navigasi Bawah Pusmendik */
